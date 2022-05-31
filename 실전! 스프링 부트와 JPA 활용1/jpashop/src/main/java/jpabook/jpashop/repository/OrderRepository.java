3d4447bbe8c92655bf598cd9be98a4361e73ac4a
김우중 <rainingdaewoo@gmail.com>
2022-05-31 11:46:01 +0900

    Add files via upload
    2022.05.31 - 주문 도메인 개발

---
+++ 실전! 스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long id) {
+        return em.find(Order.class, id);
+    }
+
+    public List<Order> findAll(OrderSearch orderSearch) {
+
+        return em.createQuery("SELECT o FROM Order o JOIN o.member m" +
+                                " WHERE o.status = :status " +
+                                " AND m.name LIKE :name", Order.class)
+                .setParameter("status", orderSearch.getStatus())
+                .setParameter("name", orderSearch.getMemberName())
+                .setMaxResults(1000)     // 최대 1000건
+                .getResultList();
+
+    }
+}
