850e154f7bb82132294165c47cf2bdade333bc18
김우중 <rainingdaewoo@gmail.com>
2022-06-05 09:29:50 +0900

    Add files via upload

--- 실전! 스프링 데이터 JPA/main/java/study/datajpa/repository/MemberRepository.java
+++ 실전! 스프링 데이터 JPA/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,12 +1,17 @@
 package study.datajpa.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.*;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import javax.persistence.LockModeType;
+import javax.persistence.QueryHint;
+import java.util.Collection;
 import java.util.List;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
@@ -22,5 +27,41 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     @Query("select new study.datajpa.dto.MemberDto(m.id, m.username, t.name) " +
             "from Member m join m.team t")
-    List<MemberDto> findMemberDto();
+    List<MemberDto> findMemberDto(); // DTO로 직접 조회
+
+    @Query("select m from Member m where m.username in :names")
+    List<Member> findByNames(@Param("names") Collection<String> names);  // 컬렉션 파라미터 바인딩 Collection 타입으로 in절 지원
+
+    List<Member> findListByUsername(String name);           //컬렉션
+    Member findMemberByUsername(String name);               //단건
+    Optional<Member> findOptionalByUsername(String name);   //단건 Optional
+
+    Page<Member> findByAge(int age, Pageable pageable);
+
+    @Modifying
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+    //공통 메서드 오버라이드
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    //JPQL + 엔티티 그래프
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    //메서드 이름으로 쿼리에서 특히 편리하다.
+    /*@EntityGraph(attributePaths = {"team"})
+    List<Member> findByUsername(String username);*/
+
+    @QueryHints(value = @QueryHint(name = "org.hibernate.readOnly", value = "true"))
+    Member findReadOnlyByUsername(String username);
+
+   /* @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Member> findByUsername(String name);*/
 }
--- 실전! 스프링 데이터 JPA/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ 실전! 스프링 데이터 JPA/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -4,10 +4,18 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,6 +27,11 @@ class MemberRepositoryTest {
 
     @Autowired
     MemberRepository memberRepository;
+    @Autowired
+    TeamRepository teamRepository;
+
+    @PersistenceContext
+    EntityManager em;
 
     @Test
     public void testMember() {
@@ -75,6 +88,87 @@ class MemberRepositoryTest {
         Assertions.assertThat(result.get(0).getUsername()).isEqualTo("AAA");
         Assertions.assertThat(result.get(0).getAge()).isEqualTo(20);
         Assertions.assertThat(result.size()).isEqualTo(1);
+    }
+
+    @Test
+    public void paging() throws Exception {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 10));
+        memberRepository.save(new Member("member3", 10));
+        memberRepository.save(new Member("member4", 10));
+        memberRepository.save(new Member("member5", 10));
+        int age = 10;
+        int offset = 0;
+        int limit = 3;
+
+        //when
+        PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC,
+                "username"));
+        Page<Member> page = memberRepository.findByAge(10, pageRequest);
+       // Page<MemberDto> dtoPage = page.map(m -> new MemberDto(m.getId(), m.getUsername(), null);
+
+        //then
+        List<Member> content = page.getContent();                       //조회된 데이터
+        Assertions.assertThat(content.size()).isEqualTo(3);             //조회된 데이터 수
+        Assertions.assertThat(page.getTotalElements()).isEqualTo(5);    //전체 데이터 수
+        Assertions.assertThat(page.getNumber()).isEqualTo(0);           //페이지 번호
+        Assertions.assertThat(page.getTotalPages()).isEqualTo(2);       //전체 페이지 번호
+        Assertions.assertThat(page.isFirst()).isTrue();                 //첫번째 항목인가?
+        Assertions.assertThat(page.hasNext()).isTrue();                 //다음 페이지가 있는가?
+    }
+
+    @Test
+    public void bulkUpdate() throws Exception {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        memberRepository.save(new Member("member2", 19));
+        memberRepository.save(new Member("member3", 20));
+        memberRepository.save(new Member("member4", 21));
+        memberRepository.save(new Member("member5", 40));
+
+        //when
+        int resultCount = memberRepository.bulkAgePlus(20);
+        em.flush();
+        em.clear();
+
+        //then
+        Assertions.assertThat(resultCount).isEqualTo(3);
+    }
+
+    @Test
+    public void findMemberLazy() throws Exception {
+        //given
+        //member1 -> teamA
+        //member2 -> teamB
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        teamRepository.save(teamA);
+        teamRepository.save(teamB);
+        memberRepository.save(new Member("member1", 10, teamA));
+        memberRepository.save(new Member("member2", 20, teamB));
+        em.flush();
+        em.clear();
+
+        //when
+        List<Member> members = memberRepository.findAll();
+
+        //then
+        for (Member member : members) {
+            member.getTeam().getName();
+        }
+    }
+
+    @Test
+    public void queryHint() throws Exception {
+        //given
+        memberRepository.save(new Member("member1", 10));
+        em.flush();
+        em.clear();
+        //when
+        Member member = memberRepository.findReadOnlyByUsername("member1");
+        member.setUsername("member2");
+        em.flush(); //Update Query 실행X
     }
 
 
