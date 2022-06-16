9ece9f3f4cd014127dade5615d9bba1625560400
김우중 <rainingdaewoo@gmail.com>
2022-06-16 16:45:47 +0900

    Add files via upload

---
+++ JWT SERVER/src/main/java/com/cos/jwt/model/User.java
@@ -0,0 +1,31 @@
+package com.cos.jwt.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Data
+@Entity
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+    private String roles; // USER, ADMIN
+
+    public List<String> getRoleList() {
+        if( this.roles.length() > 0 ) {
+            return Arrays.asList(this.roles.split("," ));
+        }
+        return new ArrayList<>();
+    }
+}
